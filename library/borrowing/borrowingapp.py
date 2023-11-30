749c6e832fefc8677a00a95d48bc3b666c6ee50f
alescagliola <132375357+alescagliola@users.noreply.github.com>
2023-11-30 05:06:28 +0100

    Add files via upload
    added corrections and some requirements

--- library/borrowing/borrowingapp.py
+++ library/borrowing/borrowingapp.py
@@ -1,94 +1,68 @@
 from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
+from bson import ObjectId
 from os import environ
-from sqlalchemy import Date
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
-db = SQLAlchemy(app)
-
-class Borrowing(db.Model):
-    __tablename__ = 'borrowing'
-
-    id = db.Column(db.Integer, primary_key=True)
-    loandate = db.Column(Date, nullable=False)
-    returndate = db.Column(Date, nullable=False)
-    clientid = db.Column(db.Integer)
-    bookid = db.Column(db.Integer)
-
-    def json(self):
-        return {'id': self.id, 'loandate': self.loandate, 'returndate': self.returndate, 'clientid': self.clientid, 'bookid': self.bookid}
-
-db.create_all()
-
+client = MongoClient(environ.get('MONGO_URI'))
+db = client.borrowingservice  # 
 
 @app.route('/test', methods=['GET'])
 def test():
-  return make_response(jsonify({'message': 'test route'}), 200)
-
+    return make_response(jsonify({'message': 'test route'}), 200)
 
 @app.route('/borrowing', methods=['POST'])
 def new_loan():
-  try:
-    data = request.get_json()
-    new_loan = Borrowing(id=data['id'], loandate=data['loandate'], returndate=data['returndate'], clientid=data['clientid'], bookid=data['bookid'])
-    db.session.add(new_loan)
-    db.session.commit()
-    return make_response(jsonify({'message': 'loan added'}), 201)
-  except Exception as e:
-    print(str(e))
-    return make_response(jsonify({'message': 'error adding the loan'}), 500)
-
+    try:
+        data = request.get_json()
+        result = db.borrowings.insert_one(data)
+        return make_response(jsonify({'message': 'loan added', 'id': str(result.inserted_id)}), 201)
+    except Exception as e:
+        print(str(e))
+        return make_response(jsonify({'message': 'error adding the loan'}), 500)
 
 @app.route('/borrowing', methods=['GET'])
 def get_loans():
-  try:
-    loans = Borrowing.query.all()
-    return make_response(jsonify([loan.json() for loan in loans]), 200)
-  except Exception as e:
-    print(str(e))
-    return make_response(jsonify({'message': 'error getting loans'}), 500)
+    try:
+        loans = list(db.borrowings.find())
+        return make_response(jsonify(loans), 200)
+    except Exception as e:
+        print(str(e))
+        return make_response(jsonify({'message': 'error getting loans'}), 500)
 
-
-@app.route('/borrowing/<int:id>', methods=['GET'])
+@app.route('/borrowing/<string:id>', methods=['GET'])
 def get_loan(id):
-  try:
-    loan = Borrowing.query.filter_by(id=id).first()
-    if loan:
-      return make_response(jsonify({'user': loan.json()}), 200)
-    return make_response(jsonify({'message': 'loan not found'}), 404)
-  except Exception as e:
-    print(str(e))
-    return make_response(jsonify({'message': 'error getting the loan'}), 500)
+    try:
+        loan = db.borrowings.find_one({'_id': ObjectId(id)})
+        if loan:
+            return make_response(jsonify(loan), 200)
+        return make_response(jsonify({'message': 'loan not found'}), 404)
+    except Exception as e:
+        print(str(e))
+        return make_response(jsonify({'message': 'error getting the loan'}), 500)
 
-
-@app.route('/borrowing/<int:id>', methods=['PUT'])
+@app.route('/borrowing/<string:id>', methods=['PUT'])
 def update_loan(id):
-  try:
-    loan = Borrowing.query.filter_by(id=id).first()
-    if loan:
-      data = request.get_json()
-      loan.loandate = data['loandate']
-      loan.returndate = data['returndate']
-      loan.clientid = data['clientid']
-      loan.bookid = data['bookid']
-      db.session.commit()
-      return make_response(jsonify({'message': 'loan updated'}), 200)
-    return make_response(jsonify({'message': 'loan not found'}), 404)
-  except Exception as e:
-    print(str(e))
-    return make_response(jsonify({'message': 'error updating the loan'}), 500)
+    try:
+        data = request.get_json()
+        result = db.borrowings.update_one({'_id': ObjectId(id)}, {'$set': data})
+        if result.modified_count > 0:
+            return make_response(jsonify({'message': 'loan updated'}), 200)
+        return make_response(jsonify({'message': 'loan not found'}), 404)
+    except Exception as e:
+        print(str(e))
+        return make_response(jsonify({'message': 'error updating the loan'}), 500)
 
-
-@app.route('/borrowing/<int:id>', methods=['DELETE'])
+@app.route('/borrowing/<string:id>', methods=['DELETE'])
 def delete_loan(id):
-  try:
-    loan = Borrowing.query.filter_by(id=id).first()
-    if loan:
-      db.session.delete(loan)
-      db.session.commit()
-      return make_response(jsonify({'message': 'loan deleted'}), 200)
-    return make_response(jsonify({'message': 'loan not found'}), 404)
-  except Exception as e:
-    print(str(e))
-    return make_response(jsonify({'message': 'error deleting the loan'}), 500)
+    try:
+        result = db.borrowings.delete_one({'_id': ObjectId(id)})
+        if result.deleted_count > 0:
+            return make_response(jsonify({'message': 'loan deleted'}), 200)
+        return make_response(jsonify({'message': 'loan not found'}), 404)
+    except Exception as e:
+        print(str(e))
+        return make_response(jsonify({'message': 'error deleting the loan'}), 500)
+
+if __name__ == '__main__':
+    app.run(debug=True)
